7475e55c73ff368661146046b704a0b13005b9a1
Kate Mills <helloworldloveksm@icloud.com>
2022-04-21 14:42:03 -0700

    copy and feature image added

--- src/pages/art-translated-for-movements.js
+++ src/pages/art-translated-for-movements.js
@@ -1,31 +1,55 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import DonateBtn from '../components/DonateBtn'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Home() {
   return (
     <Layout title="Art Translated for Movements">
       <div className="body">
-        Have fun, be smarter, challenge yourself, open up your eyes to a new
-        world, communicate with people over time, impress your friends and learn
-        an entirely new language.
+        <StaticImage
+          className="feature-image"
+          src="../images/movement-icon-to-result.jpg"
+          alt=""
+          placeholder="blurred"
+          layout="constrained"
+          format="webp"
+          maxHeight={336}
+        />
+        <h3>
+          {' '}
+          Have Fun While Identifying And Learning About Western Art Movements or
+          Styles<br />
+          (e.g. Impressionism) Where You Are - When You Want It
+        </h3>
+        You see an interesting piece of art at home or elsewhere and you wonder
+        what it means and what its place in art history is. You grab your phone
+        and capture an image of this art. Suddenly a world of interesting,
+        relevant information comes to you almost magically. Is the result right?
+        Is it wrong? Does it see things you don’t see or didn’t know? Do experts
+        agree? You get great fun and discussion with this fast, concise app that
+        entertains and also informs.
       </div>
 
       <div className="body">
-        Welcome to a new world. Try our new mobile apps leveraging new
-        technologies and mobile capabilities.
+        <h3>Be Smarter, Challenge Yourself, Impress People, Have Fun</h3>
+        Not only does this app identify the style or styles of the art work but
+        will give you relevant information about the artwork. First get the name
+        or names of the movement or possible mixes or other movements ordered by
+        how we match up, Then drill down on results to find a short guides on:
+        how to recognize and remember the movement, why it is different and
+        distinct, why it even started, why is it no longer is popular, what
+        artists best typify the style, what forms and materials are typically
+        used, what did critics say about it at the time, what are interesting,
+        unusual facts about the period and much more.
       </div>
-      <div className="body"></div>
 
-      <div className="flex-btns">
-        <DonateBtn
-          txt="Google Pay"
-          href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
-        />
-        <DonateBtn
-          txt="Paypal"
-          href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
-        />
+      <div className="body">
+        <h3>Gobbledygook On How It Works - Or Just Download It!</h3>
+        With a camera within the app the user takes a photograph and the app
+        immediately analyzes the image and identifies one or multiple possible
+        movements presented in descending order of probability if more than one.
+        The user then selects the highest probability and gets information on
+        the movement. Behind the app is the use of machine learning and writers.
       </div>
     </Layout>
   )
