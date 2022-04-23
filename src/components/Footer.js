02f225bcf7eaef2ab67a37f28e39978fdf32d29e
Kate Mills <helloworldloveksm@icloud.com>
2022-04-22 18:25:37 -0700

    Footer styled component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,14 @@ import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import DonateBtn from '../components/DonateBtn'
 
-const Footer = () => {
+import styled from 'styled-components'
+
+const Footer = ({className}) => {
   return (
-    <footer className="">
+    <FooterWrapper className={className}>
       <hr />
 
-      <div className="footer-links">
-        <Link to="/">
+        <Link to="/" className="logo">
           <StaticImage
             src="../images/logo.png"
             alt=""
@@ -17,14 +18,31 @@ const Footer = () => {
             placeholder="blurred"
           />
         </Link>
-
-        <div className="flex-btns">
           <DonateBtn txt="Give" href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/" />
           <DonateBtn txt="Google Pay" href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/" />
-        </div>
-      </div>
-    </footer>
+    </FooterWrapper>
   )
 }
+
+const FooterWrapper = styled.footer`
+  &{
+    margin-top: 2rem;
+
+    a.logo{
+      margin-top: 0;
+      margin-right: .8rem;
+      margin-bottom: .2rem;
+      margin-left: 0;
+    } 
+    a.donate-btn{
+      display:inline-block;
+      margin-top: .2rem;
+      margin-right: .8rem;
+      margin-bottom: .2rem;
+      margin-left: 0;
+    }
+  }
+
+`
 
 export default Footer
