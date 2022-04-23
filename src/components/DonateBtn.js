e056ce6a1baf81087d816185303f45585ba4c49f
Kate Mills <helloworldloveksm@icloud.com>
2022-04-22 18:25:37 -0700

    Footer styled component

--- src/components/DonateBtn.js
+++ src/components/DonateBtn.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
 
-const DonateBtn = ({txt='Mark Get On It', href="https://www.google.com"}) => {
+const DonateBtn = ({
+  txt = 'PayPal',
+  href = 'https://www.google.com',
+  className,
+}) => {
   return (
-    <a href={href} className="donate-btn">
+    <DonateButtonWrapper href={href} className={className}>
       Donate with {txt}
-    </a>
+    </DonateButtonWrapper>
   )
 }
+
+const DonateButtonWrapper = styled.a`
+  & {
+    color: white;
+    background: #055c6e;
+    border-radius: 20px;
+    padding: 10px 12.74px;
+    text-decoration: none;
+    text-align: center;
+    margin: 1rem 1rem 1.34rem;
+  }
+`
 
 export default DonateBtn
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,53 @@ import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import DonateBtn from '../components/DonateBtn'
 
-const Footer = () => {
+import styled from 'styled-components'
+
+const Footer = ({ className }) => {
   return (
-    <footer className="">
+    <FooterWrapper className={className}>
       <hr />
 
-      <div className="footer-links">
-        <Link to="/">
-          <StaticImage
-            src="../images/logo.png"
-            alt=""
-            height={40}
-            placeholder="blurred"
-          />
-        </Link>
-
-        <div className="flex-btns">
-          <DonateBtn txt="Give" href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/" />
-          <DonateBtn txt="Google Pay" href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/" />
-        </div>
-      </div>
-    </footer>
+      <Link to="/" className="logo">
+        <StaticImage
+          src="../images/logo.png"
+          alt=""
+          height={40}
+          placeholder="blurred"
+        />
+      </Link>
+      <DonateBtn
+        txt="Give"
+        href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
+        className="donate-btn"
+      />
+      <DonateBtn
+        txt="Google Pay"
+        href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
+        className="donate-btn"
+      />
+    </FooterWrapper>
   )
 }
+
+const FooterWrapper = styled.footer`
+  & {
+    margin-top: 2rem;
+
+    a.logo {
+      margin-top: 0;
+      margin-right: 0.8rem;
+      margin-bottom: 0.2rem;
+      margin-left: 0;
+    }
+    a.donate-btn {
+      display: inline-block;
+      margin-top: 0.2rem;
+      margin-right: 0.8rem;
+      margin-bottom: 0.2rem;
+      margin-left: 0;
+    }
+  }
+`
 
 export default Footer
