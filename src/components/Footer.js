979d080e6f93a250cbd12c20de71d980b769f81f
Kate Mills <helloworldloveksm@icloud.com>
2022-04-22 19:01:27 -0700

    min and max height 100%

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,7 @@ import styled from 'styled-components'
 const Footer = ({ className }) => {
   return (
     <FooterWrapper className={className}>
-      <hr />
-
+    <div className="footer-container">
       <Link to="/" className="logo">
         <StaticImage
           src="../images/logo.png"
@@ -23,20 +22,26 @@ const Footer = ({ className }) => {
         href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
         className="donate-btn"
       />
-      <DonateBtn
-        txt="Google Pay"
-        href="https://support.actblue.com/donors/contributions/how-do-i-use-google-pay-to-donate/"
-        className="donate-btn"
-      />
+    </div>
     </FooterWrapper>
   )
 }
 
 const FooterWrapper = styled.footer`
   & {
+    border-top: 1px solid #33333319;
+    padding-top: 1rem;
+    margin: 0 auto;
     margin-top: 2rem;
 
+    .footer-container{
+      display:flex;
+      flex-wrap: wrap;
+      align-items: flex-end;
+    }
+
     a.logo {
+      display:inline-block;
       margin-top: 0;
       margin-right: 0.8rem;
       margin-bottom: 0.2rem;
@@ -44,7 +49,8 @@ const FooterWrapper = styled.footer`
     }
     a.donate-btn {
       display: inline-block;
-      margin-top: 0.2rem;
+
+      margin-top: 1rem;
       margin-right: 0.8rem;
       margin-bottom: 0.2rem;
       margin-left: 0;
