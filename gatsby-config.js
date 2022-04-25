b6c8d7a2173bca91abf37d3f1981e388ece7718e
Kate Mills <helloworldloveksm@icloud.com>
2022-04-25 10:47:51 -0700

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,25 @@ require('dotenv').config({
 
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        description: `Understand the Language of Art. Welcome to a new world. Try our new mobile apps leveraging new technologies and mobile capabilities.`,
+        lang: `en`,
+        name: `Art Translated Mobile Apps`,
+        short_name: `Art Translated`,
+        start_url: `/`,
+        id: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#055c6e`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
     `gatsby-plugin-styled-components`,
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
