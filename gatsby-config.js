05b4d091ee67118fe442fb4c58f22a31f63edfeb
Kate Mills <helloworldloveksm@icloud.com>
2022-05-10 11:40:49 -0700

    move GA to the head

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-226103965-1",
+        head: true,
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         description: `Understand the Language of Art. Welcome to a new world. Try our new mobile apps leveraging new technologies and mobile capabilities.`,
@@ -39,47 +47,6 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-226103965-1",
-
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-
-        // Setting this parameter is optional
-        anonymize: false,
-
-        // Setting this parameter is also optional
-        respectDNT: false,
-        
-        // Avoids sending pageview hits from custom paths
-            //exclude: ["/preview/**", "/do-not-track/me/too/"],
-        
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-
-        // Enables Google Optimize using your container Id
-        optimizeId: "OPT-WT6VL38",
-        
-        // Enables Google Optimize Experiment ID
-        experimentId: "bqhHn5ueRzyC4rcxPkkypg",
-        
-        // Set Variation ID. 0 for original 1,2,3....
-            //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        
-        // Defers execution of google analytics script after page load
-        defer: false,
-
-        // Any additional optional fields
-            //sampleRate: 5,
-            //siteSpeedSampleRate: 10,
-            //cookieDomain: "example.com",
-        
-        // defaults to false
-        enableWebVitalsTracking: true,
       },
     },
   ],
