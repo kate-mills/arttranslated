28cc68325b964971f0867d77c9a97812eb9aeb04
Kate Mills <helloworldloveksm@icloud.com>
2022-06-14 16:29:21 -0700

    borders

--- src/pages/art-translated-for-movements.js
+++ src/pages/art-translated-for-movements.js
@@ -12,76 +12,86 @@ const MovmentsPage = () => {
         title={`Art Translated Movement Classifier`}
         description={`Have Fun While Identifying And Learning About Western Art Movements or Styles. Where You Are - When You Want It`}
       />
-      <div className="body" style={{ textAlign: 'center' }}>
-        <StaticImage
-          className="feature-image"
-          src="../images/full-page/for-movements-featureImg.jpg"
-          alt=""
-          placeholder="blurred"
-          layout="constrained"
-          format="webp"
-          height={336}
-        />
-      </div>
-      <div className="body">
-        <Link to="/movement-video">
-          Video of Art Translated Movement Classifier Mobile App
-          <GoArrow />
-        </Link>
-      </div>
-      <div className="body">
-        <h3>
-          Have Fun While Identifying And Learning About Western Art Movements or
-          Styles (e.g. Impressionism) Where You Are - When You Want It
-        </h3>
-        <p>
-          You see an interesting piece of art at home or elsewhere and you
-          wonder what it means and what is its place in art history. You grab
-          your phone and capture an image of this art. Suddenly a world of
-          interesting, relevant information comes to you almost magically. Do
-          you agree with our conclusion? Did our result tell you things you
-          didn’t notice or didn’t know? Do the experts agree? Download the app
-          and have great fun and stimulate discussion with your friends with
-          this fast, concise app that entertains and also informs you about
-          Western Art history.
-        </p>
-      </div>
+      <section>
+        <div className="section-feature-image drop-shadow">
+          <StaticImage
+            className="feature-image"
+            src="../images/full-page/for-movements-featureImg.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="constrained"
+            format="webp"
+            height={336}
+          />
+        </div>
+        <h1>Art Translated for Art Movements</h1>
+        <div className="article">
+          <p className="article-text">
+            <Link to="/movement-video">
+              Video of Art Translated Movement Classifier Mobile App
+              <GoArrow />
+            </Link>
+          </p>
+        </div>
+        <div className="article">
+          <h3 className="article-heading">
+            Have Fun While Identifying And Learning About Western Art Movements
+            or Styles (e.g. Impressionism) Where You Are - When You Want It
+          </h3>
+          <p className="article-text">
+            You see an interesting piece of art at home or elsewhere and you
+            wonder what it means and what is its place in art history. You grab
+            your phone and capture an image of this art. Suddenly a world of
+            interesting, relevant information comes to you almost magically. Do
+            you agree with our conclusion? Did our result tell you things you
+            didn’t notice or didn’t know? Do the experts agree? Download the app
+            and have great fun and stimulate discussion with your friends with
+            this fast, concise app that entertains and also informs you about
+            Western Art history.
+          </p>
+        </div>
 
-      <div className="body">
-        <h3>Be Smarter, Challenge Yourself, Impress People, Have Fun</h3>
-        <p>
-          Not only does this app identify the movements or styles of the art
-          work but will give you information to retain and learn as well.
-          Results are written as small bits of digestible information in ways it
-          can be remembered. For example, we give you an artist whose style you
-          might know from popular culture, i.e. Van Gogh, to quickly link you to
-          the movement. Or we give a few specific words of description such as
-          ‘squares, animals, lots of the color red’ to help you identify and
-          remember the movement.
-        </p>
+        <div className="article">
+          <h3 className="article-heading">
+            Be Smarter, Challenge Yourself, Impress People, Have Fun
+          </h3>
+          <p className="article-text">
+            Not only does this app identify the movements or styles of the art
+            work but will give you information to retain and learn as well.
+            Results are written as small bits of digestible information in ways
+            it can be remembered. For example, we give you an artist whose style
+            you might know from popular culture, i.e. Van Gogh, to quickly link
+            you to the movement. Or we give a few specific words of description
+            such as ‘squares, animals, lots of the color red’ to help you
+            identify and remember the movement.
+          </p>
 
-        <p>
-          First the app identifies the image’s movement or other possible
-          candidate movements. You can then click and learn more finding short
-          guides on: how to recognize and remember the movement, why it is
-          different and distinct, why it even started, why is it no longer
-          popular, what artists best typify the style, what forms and materials
-          are typically used, what did critics say about it at the time, what
-          are interesting or unusual facts about the period and much more.
-        </p>
-      </div>
+          <p className="article-text">
+            First the app identifies the image’s movement or other possible
+            candidate movements. You can then click and learn more finding short
+            guides on: how to recognize and remember the movement, why it is
+            different and distinct, why it even started, why is it no longer
+            popular, what artists best typify the style, what forms and
+            materials are typically used, what did critics say about it at the
+            time, what are interesting or unusual facts about the period and
+            much more.
+          </p>
+        </div>
 
-      <div className="body">
-        <h3>Gobbledygook On How It Works - Or Just Download It!</h3>
-        <p>
-          With a camera within the app the user takes a photograph and the app
-          immediately analyzes the image and identifies one or multiple possible
-          movements presented in descending order of probability if there are
-          more than one. The user then selects the highest probability and gets
-          information on the movement. Behind the app is the use of machine
-          learning and the writers.
-        </p>
-      </div>
+        <div className="article">
+          <h3 className="article-heading">
+            Gobbledygook On How It Works - Or Just Download It!
+          </h3>
+          <p className="article-text">
+            With a camera within the app the user takes a photograph and the app
+            immediately analyzes the image and identifies one or multiple
+            possible movements presented in descending order of probability if
+            there are more than one. The user then selects the highest
+            probability and gets information on the movement. Behind the app is
+            the use of machine learning and the writers.
+          </p>
+        </div>
+      </section>
     </Layout>
   )
 }
--- src/pages/art-translated-for-symbols.js
+++ src/pages/art-translated-for-symbols.js
@@ -6,7 +6,6 @@ import Seo from '../components/Seo'
 import { Link } from 'gatsby'
 import GoArrow from '../components/GoArrow'
 
-
 const SymbolismPage = () => {
   return (
     <Layout>
@@ -14,85 +13,90 @@ const SymbolismPage = () => {
         title="Art Translated for Symbols"
         description="You can now understand a new and unique artistic language now and over history with one of the first mobile apps to convey the meanings, intentions and feelings of symbols in Western art. This app is unique from most books and other reference sources using a structured database of over 120,000 indicators to translate this language in your hand anywhere in the world."
       />
-      <div className="body" style={{ textAlign: 'center' }}>
-        <StaticImage
-          className="feature-image"
-          src="../images/full-page/for-symbols-featureImg.jpg"
-          alt=""
-          placeholder="blurred"
-          layout="constrained"
-          format="webp"
-          height={336}
-        />
-      </div>
-      <div className="section">
-        <h3 className="section-heading">
-          Translate The Language of Symbols Using Your Mobile Phone
-        </h3>
-        <p className="section-text">
-          Better understand and learn a new language using this fun new mobile
-          app to “listen” in on the ideas, meanings, intentions and feelings
-          that artists and their patrons want you to hear from the past
-          thousands of years. This mobile app is unique from most such books and
-          other reference sources by providing a data driven experience with
-          more context and precise meanings using a structured database of over
-          120,000 indicators to put these art translations in your hand quickly
-          and concisely.
-        </p>
-      </div>
+      <section>
+        <h1>Art Translated For Symbols</h1>
+        <div className="section-feature-image drop-shadow">
+          <StaticImage
+            className="feature-image"
+            src="../images/full-page/for-symbols-featureImg.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="constrained"
+            format="webp"
+            height={336}
+          />
+        </div>
 
-      <div className="section">
-        <p className="section-text">
-          <Link
-            to="/symbol-video"
-            className="section-link"
-            title="Watch Video of the Art Translated Symbolism Mobile App">
-            Click here to see video of how it works
-    <GoArrow/>
-          </Link>
-        </p>
-      </div>
+        <div className="article">
+          <h3 className="article-heading">
+            Translate The Language of Symbols Using Your Mobile Phone
+          </h3>
+          <p className="article-text">
+            Better understand and learn a new language using this fun new mobile
+            app to “listen” in on the ideas, meanings, intentions and feelings
+            that artists and their patrons want you to hear from the past
+            thousands of years. This mobile app is unique from most such books
+            and other reference sources by providing a data driven experience
+            with more context and precise meanings using a structured database
+            of over 120,000 indicators to put these art translations in your
+            hand quickly and concisely.
+          </p>
+        </div>
 
-      <div className="body">
-        <h3>Fast, Concise Search Results Ranked By Probability</h3>
-        <p>
-          Because this app is designed specifically for a single function it is
-          quicker and more relevant than a search of the mobile internet. As you
-          view symbols from artists all day, every day in books, museum
-          collections, parks, architecture, sculpture, decor, movies, tattoos
-          and more, you can now simply enter the symbol you are seeing and find
-          the meaning. You will be aided by suggestions as you type to enter
-          what you are seeing more accurately. The search results are written to
-          be specific and brief to get you the information quickly as is
-          expected from a mobile app. Results are presented in the highest
-          probability of what you are viewing and what it means.
-        </p>
-      </div>
+        <div className="article">
+          <p className="article-text">
+            <Link
+              to="/symbol-video"
+              className="section-link"
+              title="Watch Video of the Art Translated Symbolism Mobile App"
+            >
+              Click here to see video of how it works
+              <GoArrow />
+            </Link>
+          </p>
+        </div>
 
-      <div className="body">
-        <h3>
-          Finding The Right Result Aided By An Image Library Of Over 10,000
-          Images
-        </h3>
-        <p>
-          Your results will often have multiple images of the symbol as depicted
-          in other works of art. Therefore you can visually confirm we are
-          matching well to your symbol. Giving you multiple artistic
-          interpretations of the same symbol will also let you enjoy the amazing
-          and creative ways artists interpret the same symbol. Our images are
-          often from a mobile camera so you will see symbols as you might
-          capture them in the same real setting, context, image quality and
-          viewpoint.
-        </p>
-      </div>
-      <div className="body">
-        <h3>
-          No Need For A Lot Of Reading - Just Download the App And Try It.
-        </h3>
-      </div>
+        <div className="article">
+          <h3 className="article-heading">
+            Fast, Concise Search Results Ranked By Probability
+          </h3>
+          <p className="article-text">
+            Because this app is designed specifically for a single function it
+            is quicker and more relevant than a search of the mobile internet.
+            As you view symbols from artists all day, every day in books, museum
+            collections, parks, architecture, sculpture, decor, movies, tattoos
+            and more, you can now simply enter the symbol you are seeing and
+            find the meaning. You will be aided by suggestions as you type to
+            enter what you are seeing more accurately. The search results are
+            written to be specific and brief to get you the information quickly
+            as is expected from a mobile app. Results are presented in the
+            highest probability of what you are viewing and what it means.
+          </p>
+        </div>
+        <div className="article">
+          <h3 className="article-heading">
+            Finding The Right Result Aided By An Image Library Of Over 10,000
+            Images
+          </h3>
+          <p className="article-text">
+            Your results will often have multiple images of the symbol as
+            depicted in other works of art. Therefore you can visually confirm
+            we are matching well to your symbol. Giving you multiple artistic
+            interpretations of the same symbol will also let you enjoy the
+            amazing and creative ways artists interpret the same symbol. Our
+            images are often from a mobile camera so you will see symbols as you
+            might capture them in the same real setting, context, image quality
+            and viewpoint.
+          </p>
+        </div>
+        <div className="article">
+          <h3 className="article-heading">
+            No Need For A Lot Of Reading - Just Download the App And Try It.
+          </h3>
+        </div>
+      </section>
     </Layout>
   )
 }
-
 
 export default SymbolismPage
